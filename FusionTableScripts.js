679d7bb8ef002a1e10139e2ce13d3a22f79e57e7
Roseanne Gerin <rg2875@columbia.edu>
2014-03-07 15:26:22 -0500

    Seventh Commit

--- FusionTableScripts.js
+++ FusionTableScripts.js
@@ -11,18 +11,15 @@
   
 */
 
-	console.log ("Civilian Unemployment Data ready to go");
-
 //For this project, I am adding a Google data fusion table as another way to bring data into my Google visualization.
 
 	function newDataLoaded(unemploymentData){
 		
 	console.log(unemploymentData);	
-		
-
+	
 	var gDataTable = new google.visualization.DataTable();
 	
-		
+	
 	//When I add columns, the first parameter is the data type in that column.
 	//The second parameter is the name of the column.
 		
@@ -35,7 +32,7 @@
 		
 	//I'm creating a var for the updated chart and giving it a title.
 		
-	var myUpdatedChartOptions = {
+	var myChartOptions = {
 	title: "Unemployment in the United States"
 			
 	};	
@@ -45,9 +42,8 @@
 	var myGoogleChart = new google.visualizationLineChart(document.getElementById("unemploymentChart"));
 		
 	{	
-
 		
-	myGoogleChart.draw(googleDataTable, myChartOptions);
+	myGoogleChart.draw(gDataTable, myChartOptions);
 	
 	}
 	
@@ -56,27 +52,29 @@
 	console.log("googleLoaded"); 
 	
 	//Now I'm going to load the data from a Google fusion table instead of from the json file.
-	// I'm adding in the https address with a WHERE parameter. 
+	// I'm adding in the https address with a WHERE parameter to specifiy certain years for the data. 
 	// I have to add an ">" after the "DATE", then go directly to the date '1969-12-01'.
 
-	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*FROM+1fcALUPt1J06yQQx349URVcxAqRcgZFdWDgwO932j+WHERE+DATE>'1969-12-01'&key=AIzaSyBxm3yDApl-FkVRwHUKGACLeBhMMTX2ubI", newData, "json");
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1fSVYn7aezbKW9l1Vxi93-jH46rN7x2mxbwr_-2o3&key=AIzaSyBxm3yDApl-FkVRwHUKGACLeBhMMTX2ubI", newDataLoaded, "json");
 	}
 
 	console.log("googleLoaded");
 	
+	
+	//I am working with the Google chart loading function.
 	function pageLoaded(){
 		
-	console.log("go to page loaded");
-	
 	//Now I'll load the Google vizualization library.
 	google.load("vizualization", '1', {
 	packages : ["corechart"],
 	callback : ["googleLoaded"]
+	
 });
 
-	//This will call back the document.	
-	
+		
 	}
-	$(document).ready(pageLoaded);
-
+	
+	$(document).ready(gDataTable);
+	
 }
+
